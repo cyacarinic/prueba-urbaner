4d4ba5e26c6f2dc5bd75367d5c26f3de9c31a684
Claudio Yacarini <cyacarini@rcp.pe>
2019-03-18 17:37:28 -0500

    Init client

--- client.py
+++ client.py
@@ -5,4 +5,8 @@ from bottle import get, run, static_file
 def index():
     return static_file('index.html', root=".")
 
+@get('/docs')
+def docs():
+    return static_file('docs.html', root=".")
+
 run(host='localhost', port=5000)
